544af96ac12be7450ddaf42a68d3beee65b91807
fairuz_ahmed_tuna <fairuz.ahmed@brainstation-23.com>
2024-06-21 10:59:06 +0600

    Initial Commit

---
+++ Movie23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie23
+//
+//  Created by BS00880 on 21/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
